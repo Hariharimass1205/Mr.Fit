1093c8c9ff94916ada1273b74b204c1cdeeb5ee3
Hariharan <hariharimass1205@gmail.com>
2024-11-16 17:43:40 +0530

    Stop tracking files in .gitignore

--- Backend/dist/routes/adminRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminController_1 = require("../controllers/adminController");
-const adminRouter = (0, express_1.Router)();
-adminRouter.post("/adminlogin", adminController_1.adminLogin);
-exports.default = adminRouter;
--- Backend/dist/routes/coachRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const coachController_1 = require("../controllers/coachController");
-const coachRouter = (0, express_1.Router)();
-coachRouter.post("/sendScore", coachController_1.saveScore);
-exports.default = coachRouter;
