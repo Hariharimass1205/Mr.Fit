45578177e3d72e65b7e0138a65cc54bb3aaaf4a0
Hariharan <hariharimass1205@gmail.com>
2025-01-20 14:21:41 +0530

    okay

--- frontend/utils/serverURL.ts
+++ frontend/utils/serverURL.ts
@@ -1,8 +1,8 @@
- export const SERVER_URL_USER = "https://mrfitserver.mrfit.life/user";
- export const SERVER_URL_COACH = "https://mrfitserver.mrfit.life/coach";
- export const SERVER_URL_CHAT = "https://mrfitserver.mrfit.life/chat";
- export const SERVER_URL_ADMIN = "https://mrfitserver.mrfit.life/admin";
- export const SERVER_URL_PAYMENT = "https://mrfitserver.mrfit.life/payment";
+ export const SERVER_URL_USER = "https://mr-fit.onrender.com/user";
+ export const SERVER_URL_COACH = "https://mr-fit.onrender.com/coach";
+ export const SERVER_URL_CHAT = "https://mr-fit.onrender.com/chat";
+ export const SERVER_URL_ADMIN = "https://mr-fit.onrender.com/admin";
+ export const SERVER_URL_PAYMENT = "https://mr-fit.onrender.com/payment";
  export const CLIENT_URL = "https://mrfit.life"
 
 
