a8a8dd684923e984572dbc2d6748a16bf1cce3d1
Hariharan <hariharimass1205@gmail.com>
2025-01-22 17:43:12 +0530

    cookie setting changed

--- Backend/src/controllers/userController.ts
+++ Backend/src/controllers/userController.ts
@@ -75,12 +75,16 @@ login = async(req:Request,res:Response,next:NextFunction) : Promise<void> =>{
     const loginData = {email:email,password:password}
     const {user,refreshToken,accessToken} = await this.userService.loginUser(loginData)
     res.cookie("accessToken",accessToken,{
-      sameSite:"strict",
+      sameSite:"none",
       httpOnly:false,
+      secure: true, 
+      domain:"https://mrfit.life"
     });
     res.cookie("refreshToken",refreshToken,{
-      sameSite:"strict",
+      sameSite:"none",
       httpOnly:true,
+      secure: true, 
+      domain:"https://mrfit.life"
     });
     res.status(HttpStatus.OK).json(user)
   } catch (error) {
